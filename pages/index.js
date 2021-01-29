16a59783083394ee486e78751064cf9fe05c6096
João Nunes Rios <joao@pw3.com.br>
2021-01-28 22:50:25 -0300

    aulas 2 - rotas, eslint e uns caprichos de styled components

--- pages/index.js
+++ pages/index.js
@@ -1,76 +1,92 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
 
-// react
-// function Title(params) {
-//   return <h1>{params.children}</h1>
-// }
-
-// styled component
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
 export const QuizContainer = styled.div`
-width: 100%;
-max-width: 350px;
-padding-top: 45px;
-margin: auto 10%;
-@media screen and (max-width: 500px) {
-  margin: auto;
-  padding: 15px;  
-  margin-top: 140px;
-}
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;  
+    margin-top: 140px;
+  }
 `;
 
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex:1;
-  background-size: cover;
-  background-position: center;
-  border-radius: 4px;
-  overflow: hidden;
-  h1, h2, h3 {
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 1;
-    margin-bottom: 0;
+export const Input = styled.input`
+  width: 100%;
+  padding: .3rem;
+  border-radius: 5px;
+`;
+
+export const SubmitButton = styled.button`
+  width: 100%;
+  padding: .3rem;
+  margin-top: .5rem;
+  cursor:pointer;
+  background-color: ${db.theme.colors.contrastText};
+  color:${db.theme.colors.white};
+  border:0;
+  border-right:2px solid ${db.theme.colors.primary};
+  border-bottom:2px solid ${db.theme.colors.primary};
+
+  :active{
+    border:0;
+    border-left:2px solid ${db.theme.colors.primary};
+    border-top:2px solid ${db.theme.colors.primary};
   }
-  p {
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1;
-  }
+  
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <Widget>
-            <Widget.Header>
-              <h1>{db.title}</h1>
-            </Widget.Header>
-            <Widget.Content>
-              <p>{db.description}</p>
-            </Widget.Content>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+            <form onSubmit={function (e) {
+              e.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Input
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                id="nome"
+                name="name"
+                placeholder="Escreva seu nome para começar!"
+              />
+              <SubmitButton type="submit" disabled={name.length === 0}>
+                BORA COMEÇAR
+                {name.length > 0 ? `, ${name}` : ''}
+              </SubmitButton>
+            </form>
+          </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Header>
-          <h2>Aqui vai outros quizzes</h2>
+            <h2>Aqui virão outros quizzes</h2>
           </Widget.Header>
           <Widget.Content>
             <p>Aguarde e confie</p>
           </Widget.Content>
         </Widget>
-        <Footer></Footer>
+        <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/joaofnr" />
     </QuizBackground>
-  )
+  );
 }
